688b76377593f7b0ebb585fe3918b16fccd3566c
rob <robert.stewart.m@outlook.com>
2024-07-01 00:26:19 -0500

    cleaned up files

--- home.js
+++
@@ -1,7 +0,0 @@
-const menuToggle = document.querySelector('.toggle');
-const main = document.querySelector('.main');
-menuToggle.addEventListener('click', () => {
-menuToggle.classList.toggle('active');
-main.classList.toggle('active');
-}
-);
